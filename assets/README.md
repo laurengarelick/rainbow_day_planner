7425d77c8d394b652aa6f0fedd52c3b1a6242d6a
lauren garelick <laurengarelick@gmail.com>
2020-06-08 10:55:49 -0700

    ok

---
+++ assets/README.md
@@ -0,0 +1,11 @@
+A simple, fun and bright calendar application that allows the user to save events for each hour of the day. 
+This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+This app promotes a fun and happy atmosphere to brighten even the most stressful of days.
+As an employee with a busy scedule, time management is imortant.
+
+
+When the planner is opened the current day will be displayed at the top. 
+When scrolling down, the user will be presented with timeblocks for standard business hours
+and whenvie they are viewed, each timeblock will be color coded to indicate whether it is in the past, present, or future.
+When clicking into a timeblock an event can be entered and saved in local storage.
+When the page is refreshed, the saved events persist.
---
+++ assets/script.js
@@ -0,0 +1,53 @@
+  
+$(document).ready(function() {
+
+    //Updates the current date to the id currentDay----------------------------------------    
+    var date = moment().format("dddd, MMMM Do YYYY");
+    $("#currentDay").text(date); 
+    
+    //Current, start and end time variables------------------------------------------------
+    var currentTime = moment().format("HH");
+    //-------------------------------------------------------------------------------------
+    
+    var dayHrs = 18;
+    
+    for( var i = 9; i < dayHrs; i++){ 
+        var timeBlock = $("<div class='time-block row'>");
+        var hour = $("<div class='hour col-1 col-xs-2'>");
+        var task = $("<textarea class='description col-10 col-xs-10'>");
+        task.attr("data-task", i);
+        var save = $("<button class='saveBtn col-1 col-xs-12'>").append("<i class='fas fa-save'>");
+        $(".container").append(timeBlock);
+        timeBlock.append(hour, task, save);
+        hour.append(i + ":00");
+    
+    //Time of day conditions for what class is added to each time block
+        if(currentTime > i){
+          timeBlock.addClass("past");
+          }
+          else if(currentTime == i){
+          timeBlock.addClass("present");
+          }
+          else if(currentTime < i){
+          timeBlock.addClass("future");
+          }
+          else if(currentTime >= 19){
+          timeBlock.addClass("past");
+          }
+    
+    //Get local storage--------------------------------------------------------------------
+          var tasknum = "tasks" + i;
+          var tasklist = localStorage.getItem(tasknum);
+          task.val(tasklist);  
+    }
+    
+    //Save text input on click to local storage--------------------------     ------------------
+          $(".saveBtn").on("click", function(){
+            var task = $(this).prev();
+            var tasks = task.val();
+            var tasknum = "tasks" + task.attr("data-task");
+            localStorage.setItem(tasknum, tasks);
+           
+          })
+    })
+    
